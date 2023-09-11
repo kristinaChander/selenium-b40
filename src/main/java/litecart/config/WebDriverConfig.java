fb6b684d2a9221e5f0b15cac1e7d8fa9ea3af4e1
kristinaChander <48016370+kristinaChander@users.noreply.github.com>
2023-09-11 20:39:08 +0300

    different browsers

--- src/main/java/litecart/config/WebDriverConfig.java
+++ src/main/java/litecart/config/WebDriverConfig.java
@@ -1,14 +1,35 @@
 package litecart.config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 
 @Configuration
 public class WebDriverConfig {
+
+    private static final String BROWSER_TYPE_FIREFOX_NIGHTLY = "firefox-nightly";
+
     @Bean
-    public WebDriver driver() {
-        return new ChromeDriver();
+    public WebDriver driver(@Value("${browser-type:chrome}") String browserType,
+                            @Value("${firefox-binary-path:}") String firefoxBinaryPath) {
+        WebDriverManager webDriverManager = getWebDriverManager(browserType);
+        if (!ObjectUtils.isEmpty(firefoxBinaryPath)) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setBinary(firefoxBinaryPath);
+            webDriverManager.capabilities(firefoxOptions);
+        }
+        return webDriverManager.create();
+    }
+
+    private WebDriverManager getWebDriverManager(String browserType) {
+        if (BROWSER_TYPE_FIREFOX_NIGHTLY.equals(browserType)) {
+            return WebDriverManager.getInstance(DriverManagerType.FIREFOX);
+        }
+        return WebDriverManager.getInstance(browserType);
     }
 }
